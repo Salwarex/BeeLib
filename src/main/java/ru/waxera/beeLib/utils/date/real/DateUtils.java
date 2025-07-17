22608c43ce22271efe1b7d420cc5e4f9fc376b1d
Salwarex <beemanity.server@gmail.com>
2025-07-17 13:35:38 +0500

    v1.3-SNAPSHOT-170835
    + DateUtils.getNow()

--- src/main/java/ru/waxera/beeLib/utils/date/real/DateUtils.java
+++ src/main/java/ru/waxera/beeLib/utils/date/real/DateUtils.java
@@ -7,11 +7,16 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.concurrent.TimeUnit;
 
 public class DateUtils {
+    public static Timestamp getNow(){
+        return Timestamp.valueOf(LocalDate.now().atStartOfDay());
+    }
+
     public static Timestamp getLast(DayOfWeek target){
         LocalDate now = LocalDate.now();
         LocalDate lastTarget = now.with(TemporalAdjusters.previousOrSame(target));
         return Timestamp.valueOf(lastTarget.atStartOfDay());
     }
+
     public static double getDaysBetween(Timestamp start, Timestamp end) {
         if (start == null || end == null) {
             throw new IllegalArgumentException("Timestamp не может быть null");
