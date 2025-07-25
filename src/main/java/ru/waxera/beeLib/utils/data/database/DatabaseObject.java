5f07b8e00b205a7c6a0c40fb476cfddb09ddf390
Salwarex (from lap) <beemanity.server@gmail.com>
2025-07-26 01:57:20 +0500

    v1.3.1-SNAPSHOT-252157
    some hotfixes

--- src/main/java/ru/waxera/beeLib/utils/data/database/DatabaseObject.java
+++ src/main/java/ru/waxera/beeLib/utils/data/database/DatabaseObject.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public abstract class DatabaseObject<T extends Enum<T>> {
     private List<T> updated;
-    public boolean isUpdated(T check){
-        return updated.contains(check);
+    public boolean isUpdated(T variable){
+        return updated.contains(variable);
+    }
+    protected void setUpdated(T variable){
+        updated.add(variable);
     }
 }
